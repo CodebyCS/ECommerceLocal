9074c198ea3b27e4c3263c5c98e85c42f14dad4d
Carlos Santana <codebycs@gmail.com>
2026-07-22 20:17:48 +0100

    feat: interfaces

---
+++ ECommerceLocal.Domain/Interfaces/IUserRepository.cs
@@ -0,0 +1,22 @@
+﻿using ECommerceLocal.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerceLocal.Domain.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<List<User>> GetAllAsync();
+
+        Task<User?> GetByIdAsync(string id);
+
+        Task<User?> GetByEmailAsync(string email);
+
+        Task CreateAsync(User user);
+
+        Task UpdateAsync(string id, User user);
+
+        Task DeleteAsync(string id);
+    }
+}

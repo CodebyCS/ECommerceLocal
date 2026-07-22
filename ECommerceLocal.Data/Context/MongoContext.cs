fe222e69bea1c51a0d7434e1c7b416f5e84654c4
Carlos Santana <codebycs@gmail.com>
2026-07-22 20:59:08 +0100

    feat: MongoContext

---
+++ ECommerceLocal.Data/Context/MongoContext.cs
@@ -0,0 +1,29 @@
+﻿using ECommerceLocal.Domain.Models;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerceLocal.Data.Context
+{
+    public class MongoContext
+    {
+        private readonly IMongoDatabase _database;
+
+        public MongoContext(IOptions<MongoSettings> settings)
+        {
+            var client = new MongoClient(settings.Value.ConnectionString);
+            _database = client.GetDatabase(settings.Value.DatabaseName);
+        }
+
+        public IMongoCollection<Product> Products =>
+            _database.GetCollection<Product>("products");
+
+        public IMongoCollection<User> Users =>
+            _database.GetCollection<User>("users");
+
+        public IMongoCollection<Order> Orders =>
+            _database.GetCollection<Order>("orders");
+    }
+}
